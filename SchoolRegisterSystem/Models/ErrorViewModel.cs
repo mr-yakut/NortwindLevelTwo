55a6deaae5b4a681d02784a4f24f99cee22bec7e
Muhammed Yakut <memoaskan@gmail.com>
2023-04-20 01:45:50 +0200

    Delete SchoolRegisterSystem directory

--- SchoolRegisterSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SchoolRegisterSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
